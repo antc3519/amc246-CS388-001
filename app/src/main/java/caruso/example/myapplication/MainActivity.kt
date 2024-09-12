bac01b3fb4f839aade9b18aab76b04b32742fd63
antc3519 <anth3619@gmail.com>
2024-09-12 13:52:32 -0400

    Lab 1 project

--- app/src/main/java/caruso/example/myapplication/MainActivity.kt
+++ app/src/main/java/caruso/example/myapplication/MainActivity.kt
@@ -2,7 +2,9 @@ package caruso.example.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,20 +12,36 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.helloButton)
+        val upgrade = findViewById<Button>(R.id.upgradeButton)
+        button.setOnClickListener{
+            //Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+            if(counter >= 100) {
+                upgrade.visibility = View.VISIBLE
+
+                upgrade.setOnClickListener {
+                    button.text = "Add 2"
+
+                    // Update original button to add 2 instead of `
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+
+                    // Hide upgrade button again
+                    upgrade.visibility = View.INVISIBLE
+                }
+            }
         }
 
-        val button = findViewById<Button>(R.id.helloButton)
-        button.setOnClickListener{
-            Log.d("Hello World", "Button Clicked")
-            Toast.makeText(this, "Hello back!", Toast.LENGTH_SHORT).show()
-        }
     }
 }
