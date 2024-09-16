2872deaf728be4ba033bbde28310032181a1c532
antc3519 <anth3619@gmail.com>
2024-09-16 17:30:11 -0400

    project 1 - wordle implementation

--- README.md
+++ README.md
@@ -1,31 +1,41 @@
-# Android Prework - *Hello World*
+# Android Project 1 - *Wordlie*
 
-Submitted by: **Anthony Caruso**
+Submitted by: **Anthony Carsuo**
 
-**Hello World** is an android app that shows an image and introductory message, and allows pressing a button to display a Toast. 
+**Wordlie** is an android app that recreates a simple version of the popular word game [Wordle](https://www.nytimes.com/games/wordle/index.html).
 
-Time spent: **1** hours spent in total
+Time spent: **X** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-* [x] Image and introductory message displayed on screen
-* [x] Button displayed on screen
-* [x] Toast with message appears when button is pressed 
+- [ ] **User has 3 chances to guess a random 4 letter word**
+- [ ] **After 3 guesses, user should no longer be able to submit another guess**
+- [ ] **After each guess, user sees the "correctness" of the guess**
+- [ ] **After all guesses are taken, user can see the target word displayed**
 
 The following **optional** features are implemented:
+
+- [ ] User can toggle betweeen different word lists
+- [ ] User can see the 'correctness' of their guess through colors on the word
+- [ ] User sees a visual change after guessing the correct word
+- [ ] User can tap a 'Reset' button to get a new word and clear previous guesses
+- [ ] User will get an error message if they input an invalid guess
+- [ ] User can see a 'streak' record of how many words they've guessed correctly.
+
+The following **additional** features are implemented:
 
 * [ ] List anything else that you can get done to improve the app functionality!
 
 ## Video Walkthrough
 
-Here's a walkthrough of implemented features:
+Here's a walkthrough of implemented user stories:
 
 <img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 <!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
+GIF created with ...
 <!-- Recommended tools:
 [Kap](https://getkap.co/) for macOS
 [ScreenToGif](https://www.screentogif.com/) for Windows
--- app/src/main/java/caruso/example/myapplication/MainActivity.kt
+++ app/src/main/java/caruso/example/myapplication/MainActivity.kt
@@ -1,13 +1,23 @@
 package caruso.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.color
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +30,96 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val button = findViewById<Button>(R.id.helloButton)
+        var wordToGuess = FourLetterWordList.getRandomFourLetterWord().lowercase()
+        val input = findViewById<TextInputEditText>(R.id.input)
+        val reset = findViewById<Button>(R.id.reset)
+        val button = findViewById<Button>(R.id.guess)
+        val textView = findViewById<TextView>(R.id.textView)
+        val finalWord = findViewById<TextView>(R.id.wordlie)
+        finalWord.text = wordToGuess
+        var guessCount = 0
+        val star1 = findViewById<ImageView>(R.id.star1)
+        val star2 = findViewById<ImageView>(R.id.star2)
+
         button.setOnClickListener{
-            Log.d("Hello World", "Button Clicked")
-            Toast.makeText(this, "Hello back!", Toast.LENGTH_SHORT).show()
+            val guess = input.text.toString().lowercase()
+            if (guess.length != 4)
+                Toast.makeText(it.context,"Guess is not exactly 4 letter!",Toast.LENGTH_SHORT).show()
+            else if (!guess.all { it.isLetter() })
+                Toast.makeText(it.context,"Guess is not only letters!",Toast.LENGTH_SHORT).show()
+            else {
+                guessCount += 1
+                val newText =
+                    textView.text.toString() + "Guess $guessCount: $guess \nGuess $guessCount Check: " + checkGuess(
+                        guess,
+                        wordToGuess
+                    ) + "\n"
+                val spanString = SpannableString(newText)
+
+                var index = 0
+                for (char in newText) {
+                    if (char == 'O')
+                        spanString.setSpan(
+                            ForegroundColorSpan(Color.GREEN),
+                            index,
+                            index + 1,
+                            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    else if (char == 'X')
+                        spanString.setSpan(
+                            ForegroundColorSpan(Color.RED),
+                            index,
+                            index + 1,
+                            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    else if (char == '+')
+                        spanString.setSpan(
+                            ForegroundColorSpan(Color.YELLOW),
+                            index,
+                            index + 1,
+                            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    index++
+                }
+                if (wordToGuess == guess) {
+                    star1.visibility = View.VISIBLE
+                    star2.visibility = View.VISIBLE
+                    guessCount = 3
+                }
+                textView.text = spanString
+                if (guessCount == 3) {
+                    finalWord.visibility = View.VISIBLE
+                    button.visibility = View.INVISIBLE
+                    reset.visibility = View.VISIBLE
+                }
+            }
         }
+
+        reset.setOnClickListener{
+            wordToGuess = FourLetterWordList.getRandomFourLetterWord().lowercase()
+            finalWord.text = wordToGuess
+            guessCount = 0
+            star1.visibility = View.INVISIBLE
+            star2.visibility = View.INVISIBLE
+            finalWord.visibility = View.INVISIBLE
+            button.visibility = View.VISIBLE
+            reset.visibility = View.INVISIBLE
+            textView.text = ""
+        }
+    }
+    private fun checkGuess(guess: String, wordToGuess: String) : SpannableStringBuilder {
+        var result = SpannableStringBuilder()
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result.color(Color.rgb(66, 245, 78), { append('O') } )
+            }
+            else if (guess[i] in wordToGuess) {
+                result.color(Color.rgb(242, 239, 29), { append('+') } )
+            }
+            else {
+                result.color(Color.rgb(242, 36, 29), { append('X') } )
+            }
+        }
+        return result
     }
 }
